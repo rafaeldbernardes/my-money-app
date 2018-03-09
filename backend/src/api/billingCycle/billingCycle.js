9cfc648853c1738463b6f468b0204d995f53a850
Rafael Delbel <rafaeldbernardes@gmail.com>
2018-03-09 20:29:27 -0300

    Added mongoose error handler and cors handler

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -7,8 +7,8 @@ const creditSchema = new mongoose.Schema({
 })
 
 const debtSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, min: 0 , rquired: true},
+    name: { type: String, required: [true, 'Name is required'] },
+    value: { type: Number, min: 0 , required: true},
     status: { type: String, required: false, uppercase: true,
         enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
 })
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,3 +2,11 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+mongoose.Error.messages.general.required = "Attribute '{PATH}' is required."
+mongoose.Error.messages.Number.min = 
+    "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = 
+    "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = 
+    "'{VALUE}' não é válido para o atributo '{PATH}'."
