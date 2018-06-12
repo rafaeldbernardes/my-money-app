d272d10048259864d4ab503c4aafca904d147679
Rafael Delbel <rafaeldbernardes@gmail.com>
2018-06-12 20:53:17 -0300

    Added common templates

---
+++ front-end/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './main/app'
+
+const element = <h1>Hello, world</h1>
+
+ReactDOM.render(<App />, document.getElementById("app"));
